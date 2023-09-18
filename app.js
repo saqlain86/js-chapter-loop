b778f90b7c4d0937da1c5e5495c6c34badfb2587
saqlain86 <143820913+saqlain86@users.noreply.github.com>
2023-09-18 22:20:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+let emptyArray = [];
+let matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+  ];
+  for (let i = 1; i <= 10; i++) {
+    console.log(i);
+  }
+  const tableNumber = parseInt(prompt("Enter the table number:"));
+  const tableLength = parseInt(prompt("Enter the table length:"));
+  
+  for (let i = 1; i <= tableLength; i++) {
+    console.log(`${tableNumber} x ${i} = ${tableNumber * i}`);
+  }
+  const fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+
+  for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+  }
+        
